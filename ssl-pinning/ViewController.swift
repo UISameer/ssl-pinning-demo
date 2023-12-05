12c771648229abf3be2b987e192fa07badb013ac
Sameer Jagtap <jagtap.sameer00@gmail.com>
2023-12-05 20:56:29 +0530

    Added Project

---
+++ ssl-pinning/ViewController.swift
@@ -0,0 +1,71 @@
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+    @IBOutlet var tableView: UITableView!
+    
+    private var selectedUser: User?
+    
+    var users: [User] = [] {
+        didSet {
+            tableView.isHidden = false
+            tableView.reloadData()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Stack Overflow Users"
+        
+        tableView.isHidden = true
+        tableView.dataSource = self
+        
+        NetworkClient.request(Router.users)
+            .responseDecodable { (response: DataResponse<UserList, AFError>) in
+                switch response.result {
+                case .success(let value):
+                    self.users = value.users
+                case .failure(let error):
+                    self.presentError(withTitle: "Oops!", message: error.localizedDescription)
+                }
+            }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetailSegue",
+           let destination = segue.destination as? DetailViewController,
+           let cell = sender as? UITableViewCell,
+           let indexPath = tableView.indexPath(for: cell) {
+            destination.user = users[indexPath.item]
+            cell.isSelected = false
+        }
+    }
+    
+    func presentError(withTitle title: String,
+                      message: String,
+                      actions: [UIAlertAction] = [UIAlertAction(title: "OK", style: .default)]) {
+        let alertController = UIAlertController(title: title,
+                                                message: message,
+                                                preferredStyle: .alert)
+        actions.forEach { action in
+            alertController.addAction(action)
+        }
+        present(alertController, animated: true)
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell",
+                                                 for: indexPath)
+        cell.textLabel?.text = users[indexPath.item].displayName
+        return cell
+    }
+}
---
+++ ssl-pinningUITests/ssl_pinningUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ssl_pinningUITestsLaunchTests.swift
+//  ssl-pinningUITests
+//
+//  Created by Sameer Personal on 12/5/23.
+//
+
+import XCTest
+
+final class ssl_pinningUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
